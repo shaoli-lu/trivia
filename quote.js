011500a29433c4d2b7ebc707e8cd7dbcb866b5a0
shaoli-lu <shaoli_lu@yahoo.com>
2022-08-17 20:49:01 -0500

    Undo the change

--- quote.js
+++ quote.js
@@ -10,7 +10,7 @@ function getQuote() {
     
    
         document.querySelector('#question').innerHTML = '"' + data.results[0].question  + '"' ;  
-        document.querySelector('#category').innerHTML = "" +  data.results[0].category;  
+        document.querySelector('#category').innerHTML = "Category: " +  data.results[0].category;  
         document.querySelector('#type').innerHTML = "Type: " +  data.results[0].type;  
         document.querySelector('#difficulty').innerHTML = "Difficulty: " +  data.results[0].difficulty;  
         document.querySelector('#correct_answer').innerHTML = "Answer: " + data.results[0].correct_answer;  
